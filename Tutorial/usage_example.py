1a13f51a866ee021275bda0be312f737a126682e
Christian <aerosetzer@gmail.com>
2022-06-27 14:07:33 +0200

    Checkpoint for home work

--- Tutorial/usage_example.py
+++ Tutorial/usage_example.py
@@ -1,13 +1,11 @@
 from bnspopkne.kne import saee_bns_emgw_with_viewing_angle as saeev
 
-
 test_inst = saeev(
     m1=1.0,
     m2=1.5,
     EOS="sfho",
     EOS_path="~/Documents/Project1_kNe/kne_modeling/eos_data/",
     kappa_grid_path="~/Documents/Project1_kNe/kne_modeling/korobkin_heating_rates/outputs/thresholded_uncertainties_20_opacity_df_020221.csv",
-    hyperparam_file="../data/paper_matern52_hyperparameters.npy",
 )
 
 # List out the parameters of the BNS mergers kilonova and binary inspiral
