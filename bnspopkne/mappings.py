c9759997a80734f4e8bab7aa655e2cb9e92bf721
Christian <aerosetzer@gmail.com>
2022-06-30 21:17:20 +0200

    More debugging

--- bnspopkne/mappings.py
+++ bnspopkne/mappings.py
@@ -216,7 +216,9 @@ def map_kne_to_grey_opacity_via_gaussian_process(
         v_ej_pred = v_ej
         Y_e_pred = Y_e
     else:
+        print(grey_opacity.shape)
         ind = np.isnan(grey_opacity)
+        print(ind.shape, m_ej_pred.shape)
         m_ej_pred = m_tot[ind]
         v_ej_pred = v_ej[ind]
         Y_e_pred = Y_e[ind]
