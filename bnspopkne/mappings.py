a94cc39663f295d4587d2b4d5c0eb58aab6f8fca
Christian <aerosetzer@gmail.com>
2022-07-01 12:21:08 +0200

    Choose correct debug variable

--- bnspopkne/mappings.py
+++ bnspopkne/mappings.py
@@ -218,7 +218,7 @@ def map_kne_to_grey_opacity_via_gaussian_process(
     else:
         print(grey_opacity.shape)
         ind = np.isnan(grey_opacity)
-        print(ind.shape, m_ej_pred.shape)
+        print(ind.shape, m_tot.shape)
         m_ej_pred = m_tot[ind]
         v_ej_pred = v_ej[ind]
         Y_e_pred = Y_e[ind]
