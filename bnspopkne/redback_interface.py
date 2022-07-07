c27d3da604d14bc269520d6be094be5a37274fc8
Christian <aerosetzer@gmail.com>
2022-07-07 22:19:42 +0200

    Start trying to interface kne model with redback

--- bnspopkne/redback_interface.py
+++ bnspopkne/redback_interface.py
@@ -1,11 +1,12 @@
+import numpy as np
 from astropy import units as u
 from kne import saee_bns_emgw_with_viewing_angle as saee_pop_view
 
 
 def redback_S22_BNS_popkNe(
     time,
-    wavelengths: np.ndarray,
-    output_unit: astropy.units.UnitBase = u.erg / (u.cm ** 2 * u.s * u.Angstrom),
+    redshift,
+    frequency: np.ndarray,
     **kwargs
 ):
     """
@@ -16,6 +17,10 @@ def redback_S22_BNS_popkNe(
         time: float
             The time at which to retrieve the flux density for the given transient
             instance.
+        redshift: float
+            The redshift of the transients
+        frequency: np.ndarray
+            Frequencies to evaluate.
         mass1: float
             The source frame mass of the primary component neutron star in solar
             masses.
@@ -29,8 +34,17 @@ def redback_S22_BNS_popkNe(
             Dictionary of other optional model parameters to be passed to the
             transient generator.
     """
+    # convert shapes
+    time_u = np.unique(time)
+    freq_u = np.unique(frequency)
+    wavelengths = (freq_u*u.Hz).to(u.Ang, equivalenceies=u.spectral()).value
     kne = saee_pop_view(kwargs)
     flux_density = (
-        kne.model.flux(time, wavelengths) * u.erg / (u.cm ** 2 * u.s * u.Angstrom)
+        kne.model.flux(time_u, wavelengths) * u.erg / (u.cm ** 2 * u.s * u.Angstrom)
     )
-    return flux_density.to(output_unit).value
+    flux_density_io = flux_density.to(output_unit).value
+    redback_flux = flux_density_io.reshape(-1)
+    if kwargs['output_format'] == 'flux_density':
+        return (flux_density* u.erg / (u.cm ** 2 * u.s * u.Angstrom)).to(u.mJy, equivalenceies=u.spectral_density(wavelengths)).value
+    elif kwargs['output_format'] == 'magnitude':
+        return flux_density.to(u.ABmag).value
