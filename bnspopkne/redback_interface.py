8d62a4b58669076b722108a3a2250a6d70c23235
Christian <aerosetzer@gmail.com>
2022-07-08 13:56:08 +0200

    Test out redback interface

--- bnspopkne/redback_interface.py
+++ bnspopkne/redback_interface.py
@@ -3,12 +3,7 @@ from astropy import units as u
 from kne import saee_bns_emgw_with_viewing_angle as saee_pop_view
 
 
-def redback_S22_BNS_popkNe(
-    time,
-    redshift,
-    frequency: np.ndarray,
-    **kwargs
-):
+def redback_S22_BNS_popkNe(time, redshift, frequency: np.ndarray, **kwargs):
     """
     Wrapper for the model to interface with redback fitting software.
 
@@ -37,14 +32,28 @@ def redback_S22_BNS_popkNe(
     # convert shapes
     time_u = np.unique(time)
     freq_u = np.unique(frequency)
-    wavelengths = (freq_u*u.Hz).to(u.Ang, equivalenceies=u.spectral()).value
-    kne = saee_pop_view(kwargs)
+    kwargs["z"] = redshift
+    wavelengths = (freq_u * u.Hz).to(u.Ang, equivalenceies=u.spectral()).value
+
+    kne = saee_pop_view(
+        kwargs,
+        min_wave=wavelengths.min(),
+        max_wave=wavelengths.max(),
+        sim_gw=False,
+        EOS="sfho",
+        EOS_path="/cfs/data/chse9649/input_data/kne_data/eos_data/",
+        kappa_grid_path="/cfs/data/chse9649/input_data/kne_data/opacity_data/Setzer2022_popkNe_opacities.csv",
+        gp_hyperparameter_file="/cfs/data/chse9649/input_data/kne_data/opacity_data/Setzer2022_popkNe_GP_hyperparam.npy",
+    )
     flux_density = (
         kne.model.flux(time_u, wavelengths) * u.erg / (u.cm ** 2 * u.s * u.Angstrom)
     )
-    flux_density_io = flux_density.to(output_unit).value
-    redback_flux = flux_density_io.reshape(-1)
-    if kwargs['output_format'] == 'flux_density':
-        return (flux_density* u.erg / (u.cm ** 2 * u.s * u.Angstrom)).to(u.mJy, equivalenceies=u.spectral_density(wavelengths)).value
-    elif kwargs['output_format'] == 'magnitude':
-        return flux_density.to(u.ABmag).value
+    redback_flux = flux_density.reshape(-1)
+    if kwargs["output_format"] == "flux_density":
+        return (
+            (redback_flux * u.erg / (u.cm ** 2 * u.s * u.Angstrom))
+            .to(u.mJy, equivalenceies=u.spectral_density(wavelengths))
+            .value
+        )
+    elif kwargs["output_format"] == "magnitude":
+        return redback_flux.to(u.ABmag).value
