0007d5ab3da27afb4c1b2bf1b0419ee2cf3faf8d
Christian <aerosetzer@gmail.com>
2022-09-27 17:10:17 +0200

    test additions

--- tests/test_eos.py
+++ tests/test_eos.py
@@ -1,3 +1,24 @@
+"""
+ Module to test the functionality of handling the equation of state in equation_of_state.py
+"""
+
 import unittest
 from bnspopkne import equation_of_state as eos
 from bnspopkne.kne import Setzer2022_kilonova as kilonova
+
+
+class test_equation_of_state(unittest.TestCase):
+    def test_get_EOS_table(self):
+
+
+    def test_radius_interpolator_from_EOS(self):
+
+
+    def test_max_EOS_mass(self):
+
+
+    def test_compactness_from_EOS(self):
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_kne.py
+++ tests/test_kne.py
@@ -1,3 +1,7 @@
+"""
+ Module to test implementations of kne.py module
+"""
+
 import unittest
 from bnspopkne.kne import Setzer2022_kilonova as kilonova
 from bnspopkne.kne import Setzer2022_kilonova as population
@@ -6,9 +10,17 @@ from bnspopkne.kne import Setzer2022_kilonova as population
 class test_kne(unittest.TestCase):
     def test_kilonova(self):
         kilo_instance = kilonova()
+        self.assertIsInstance(kilo_instance, kilonova)
 
     def test_kilonova_population(self):
+        paper_ranges = {}
+        paper_properties = {}
         pop_instance = population()
+        for key, value in paper_ranges.items():
+            getattr(pop_instance, key)
+
+    @unittest.skipIf
+    def test_derived_properties_of_populations
 
 
 if __name__ == '__main__':
