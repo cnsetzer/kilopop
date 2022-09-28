24a7869758fd0d7c68a67d9535de1c1d9c85339f
Christian <aerosetzer@gmail.com>
2022-09-28 17:00:43 +0200

    Framework for macronova wrapper tests

--- tests/test_macronova_wrapper.py
+++ tests/test_macronova_wrapper.py
@@ -1,2 +1,22 @@
 import unittest
 from bnspopkne import macronovae_wrapper as mw
+
+
+class test_macronova_engine(unittest.TestCase):
+    def setUp(self):
+
+    def test_planck_function():
+        lamdba_cm = np.array(3.0*1e-5)  # wavelength of 3000 Ang in cm
+        temperature = np.array(10000)  # K
+        result = mw.compute_planck_function(lambda_cm, temperature)
+        
+
+    def test_sed_timeseries():
+
+
+    def test_create_seds():
+
+
+
+if __name__ == '__main__':
+    unittest.main()
